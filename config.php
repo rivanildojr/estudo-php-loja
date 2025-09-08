d70106bf596e4108dff2cd3ce1322814a0dbbbcf
Rivanildo Júnior dos Santos Andrade <rivanildo_junior7@hotmail.com>
2025-09-08 10:04:10 -0300

    iniciando classe de conexão a base de dados

--- config.php
+++ config.php
@@ -2,3 +2,10 @@
 
     define('APP_NAME', 'STORE');
     define('APP_VERSION', '1.0.0');
+
+    // MYSQL
+    define("MYSQL_HOST", "localhost");
+    define("MYSQL_DATABASE", "store");
+    define("MYSQL_USER", "root");
+    define("MYSQL_PASS", "");
+    define("MYSQL_CHARSET", "utf8mb4");
--- core/classes/Database/Database.php
+++ core/classes/Database/Database.php
@@ -2,12 +2,61 @@
 
     namespace core\classes\Database;
 
+    use PDO;
+    use PDOException;
+
     /**
      * Classe responsável pela conexão com o banco de dados
      */
     class Database {
 
-        public function __construct() {
-            echo 'Base de dados!';
+        private $connection;
+
+        /**
+         * Função de conexão com o banco de dados
+         */
+        private function connect() {
+            $this->connection = new PDO(
+                'mysql:' .
+                'host=' . MYSQL_HOST . ';' .
+                'dbname=' . MYSQL_DATABASE . ';' .
+                'charset=' . MYSQL_CHARSET,
+                MYSQL_USER,
+                MYSQL_PASS,
+                array(PDO::ATTR_PERSISTENT => true)
+            );
+
+            // Configura o modo de erro do PDO
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+        }
+
+        /**
+         * Função de desconexão com o banco de dados
+         */
+        private function disconnect() {
+            $this->connection = null;
+        }
+
+        /**
+         * Função de seleção de dados
+         */
+        public function select($sql, $params = null) {
+            $this->connect();
+
+            $result = null;
+
+            try {
+                $stmt = $this->connection->prepare($sql);
+
+                !empty($params) ? $stmt->execute($params) : $stmt->execute();
+
+                $result = $stmt->fetchAll(PDO::FETCH_CLASS);
+            } catch (PDOException $e) {
+                return false;
+            } finally {
+                $this->disconnect();
+            }
+
+            return $result;
         }
     }
