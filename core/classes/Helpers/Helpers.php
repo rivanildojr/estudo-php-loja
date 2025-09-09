1ab940c7df435cc8bee2a52db1c286fda6b2949e
Rivanildo Júnior dos Santos Andrade <rivanildo_junior7@hotmail.com>
2025-09-09 08:59:40 -0300

    criando uma gestão de views

--- core/classes/Helpers/Helpers.php
+++ core/classes/Helpers/Helpers.php
@@ -2,12 +2,24 @@
 
     namespace core\classes\Helpers;
 
+    use Exception;
+
     /**
      * Classe responsável por auxiliar em diversas funções
      */
     class Helpers {
 
-        public function teste() {
-            echo 'Olá!';
+        public static function Layout($structures, $data = null) {
+            if (!is_array($structures)) {
+                throw new Exception("A coleção deve ser um array");
+            }
+
+            if (!empty($data) && is_array($data)) {
+                extract($data);
+            }
+
+            foreach ($structures as $structure) {
+                include("../core/views/$structure.php");
+            }
         }
     }
--- core/controllers/Main/Main.php
+++ core/controllers/Main/Main.php
@@ -2,9 +2,19 @@
 
     namespace core\controllers\Main;
 
+use core\classes\Helpers\Helpers;
+
     class Main {
         public function index() {
-            echo 'Página Inicial';
+            $data = [
+                'title' => 'Hello World!'
+            ];
+
+            Helpers::Layout([
+                'components/Header/index',
+                'pages/Home/index',
+                'components/Footer/index'
+            ], $data);
         }
 
         public function store() {
